b018e8157198a54afd88a2867932505cd3277c3c
EphremAlemnew <ephremalemnew@gmail.com>
2025-01-21 14:50:21 +0300

    Browse by products update

---
+++ src/pages/BrowseByProducts.jsx
@@ -0,0 +1,344 @@
+import {
+  Button,
+  HStack,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import React from "react";
+import {
+  BiData,
+  BiEqualizer,
+  BiInjection,
+  BiLock,
+  BiShare,
+  BiStar,
+  BiToggleLeft,
+  BiTrafficCone,
+} from "react-icons/bi";
+import { BsFillMarkdownFill, BsMarkdown, BsMarkerTip } from "react-icons/bs";
+import { CiMonitor } from "react-icons/ci";
+import { FaMarker } from "react-icons/fa";
+import { FaRadiation } from "react-icons/fa6";
+import { HiMiniBellAlert, HiSpeakerWave } from "react-icons/hi2";
+import {
+  IoAlert,
+  IoCheckmarkCircle,
+  IoLockClosed,
+  IoSettings,
+} from "react-icons/io5";
+import { LuLink } from "react-icons/lu";
+import {
+  MdDataUsage,
+  MdEqualizer,
+  MdMobiledataOff,
+  MdTraffic,
+} from "react-icons/md";
+
+const BrowseByProducts = () => {
+  return (
+    <VStack my={"16"} alignItems={"start"}>
+      <Text fontWeight={"bolder"} fontSize={"3xl"}>
+        Browse by Products
+      </Text>
+      <Stack
+        w={"full"}
+        borderBottom={"sm"}
+        borderBottomColor={"gray.200"}
+        py={"10"}
+        direction={{ base: "column", md: "row" }}
+      >
+        <VStack w={"1/2"} alignItems={"start"}>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            Scale
+          </Text>
+          <Text color={"gray.500"}>
+            Scale data Streaming with security and control
+          </Text>
+        </VStack>
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+          }}
+          gap={4}
+          p={"4"}
+          w={"full"}
+        >
+          <Link
+            rounded={"md"}
+            border={"sm"}
+            borderColor={"gray.200"}
+            fontWeight={"bold"}
+            textDecoration={"none"}
+          >
+            <Text
+              p={"3"}
+              roundedRight={"lg"}
+              rounded={"sm"}
+              color={"white"}
+              bg={"#17333b"}
+            >
+              <IoSettings />
+            </Text>
+            <Text>Configuration</Text>
+          </Link>
+          <Link
+            rounded={"md"}
+            border={"sm"}
+            borderColor={"gray.200"}
+            fontWeight={"bold"}
+            textDecoration={"none"}
+          >
+            <Text
+              p={"3"}
+              roundedRight={"lg"}
+              rounded={"sm"}
+              color={"white"}
+              bg={"#17333b"}
+            >
+              <IoCheckmarkCircle />
+            </Text>
+            <Text>Self-service Quickstart</Text>
+          </Link>
+          <Link
+            rounded={"md"}
+            border={"sm"}
+            borderColor={"gray.200"}
+            fontWeight={"bold"}
+            textDecoration={"none"}
+          >
+            <Text
+              p={"3"}
+              roundedRight={"lg"}
+              rounded={"sm"}
+              color={"white"}
+              bg={"#17333b"}
+            >
+              <IoLockClosed />
+            </Text>
+            <Text>RBAC</Text>
+          </Link>
+          <Link
+            rounded={"md"}
+            border={"sm"}
+            borderColor={"gray.200"}
+            fontWeight={"bold"}
+            textDecoration={"none"}
+          >
+            <Text
+              p={"3"}
+              roundedRight={"lg"}
+              rounded={"sm"}
+              color={"white"}
+              bg={"#17333b"}
+            >
+              <MdEqualizer />
+            </Text>
+            <Text>Kafka Resource Management</Text>
+          </Link>
+          <Link
+            rounded={"md"}
+            border={"sm"}
+            borderColor={"gray.200"}
+            fontWeight={"bold"}
+            textDecoration={"none"}
+          >
+            <Text
+              p={"3"}
+              roundedRight={"lg"}
+              rounded={"sm"}
+              color={"white"}
+              bg={"#17333b"}
+            >
+              <HiMiniBellAlert />
+            </Text>
+            <Text>Monitor & Alerting</Text>
+          </Link>
+          <Link
+            rounded={"md"}
+            border={"sm"}
+            borderColor={"gray.200"}
+            fontWeight={"bold"}
+            textDecoration={"none"}
+          >
+            <Text
+              p={"3"}
+              roundedRight={"lg"}
+              rounded={"sm"}
+              color={"white"}
+              bg={"#17333b"}
+            >
+              <MdTraffic />
+            </Text>
+            <Text>Traffic Control Policies</Text>
+          </Link>
+        </SimpleGrid>
+      </Stack>
+      <Stack
+        w={"full"}
+        borderBottom={"sm"}
+        borderBottomColor={"gray.200"}
+        py={"10"}
+        direction={{ base: "column", md: "row" }}
+      >
+        <VStack w={"1/2"} alignItems={"start"}>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            Shield
+          </Text>
+          <Text color={"gray.500"}>
+            Protect sensitive data and adhere to regulations
+          </Text>
+        </VStack>
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+          }}
+          gap={4}
+          p={"4"}
+          w={"full"}
+        >
+          <Link
+            rounded={"md"}
+            border={"sm"}
+            borderColor={"gray.200"}
+            fontWeight={"bold"}
+            textDecoration={"none"}
+          >
+            <Text
+              p={"3"}
+              roundedRight={"lg"}
+              rounded={"sm"}
+              color={"white"}
+              bg={"#17333b"}
+            >
+              <BiLock />
+            </Text>
+            <Text>Encryption</Text>
+          </Link>
+          <Link
+            rounded={"md"}
+            border={"sm"}
+            borderColor={"gray.200"}
+            fontWeight={"bold"}
+            textDecoration={"none"}
+          >
+            <Text
+              p={"3"}
+              roundedRight={"lg"}
+              rounded={"sm"}
+              color={"white"}
+              bg={"#17333b"}
+            >
+              <LuLink />
+            </Text>
+            <Text>Data Masking</Text>
+          </Link>
+          <Link
+            rounded={"md"}
+            border={"sm"}
+            borderColor={"gray.200"}
+            fontWeight={"bold"}
+            textDecoration={"none"}
+          >
+            <Text
+              p={"3"}
+              roundedRight={"lg"}
+              rounded={"sm"}
+              color={"white"}
+              bg={"#17333b"}
+            >
+              <BsFillMarkdownFill />
+            </Text>
+            <Text>Audit</Text>
+          </Link>
+          <Link
+            rounded={"md"}
+            border={"sm"}
+            borderColor={"gray.200"}
+            fontWeight={"bold"}
+            textDecoration={"none"}
+          >
+            <Text
+              p={"3"}
+              roundedRight={"lg"}
+              rounded={"sm"}
+              color={"white"}
+              bg={"#17333b"}
+            >
+              <BiInjection />
+            </Text>
+            <Text>Dynamic Header Injection</Text>
+          </Link>
+        </SimpleGrid>
+      </Stack>
+      <Stack
+        w={"full"}
+        borderBottom={"sm"}
+        borderBottomColor={"gray.200"}
+        py={"10"}
+        direction={{ base: "column", md: "row" }}
+      >
+        <VStack w={"1/2"} alignItems={"start"}>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            Exchange
+          </Text>
+          <Text color={"gray.500"}>
+            Grow your business with secure data sharing
+          </Text>
+        </VStack>
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+          }}
+          gap={4}
+          p={"4"}
+          w={"full"}
+        >
+          <Link
+            rounded={"md"}
+            border={"sm"}
+            borderColor={"gray.200"}
+            fontWeight={"bold"}
+            textDecoration={"none"}
+          >
+            <Text
+              p={"3"}
+              roundedRight={"lg"}
+              rounded={"sm"}
+              color={"white"}
+              bg={"#17333b"}
+            >
+              <BiShare />
+            </Text>
+            <Text>Third-Party Data Sharing</Text>
+          </Link>
+          <Link
+            rounded={"md"}
+            border={"sm"}
+            borderColor={"gray.200"}
+            fontWeight={"bold"}
+            textDecoration={"none"}
+          >
+            <Text
+              p={"3"}
+              roundedRight={"lg"}
+              rounded={"sm"}
+              color={"white"}
+              bg={"#17333b"}
+            >
+              <BiToggleLeft />
+            </Text>
+            <Text>Partner Zones</Text>
+          </Link>
+        </SimpleGrid>
+      </Stack>
+    </VStack>
+  );
+};
+
+export default BrowseByProducts;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   Box,
   VStack,
   Text,
   Stack,
   Image,
-  Blockquote,
-  BlockquoteIcon,
-  Float,
-  Heading,
-  HStack,
   Button,
+  SimpleGrid,
 } from "@chakra-ui/react";
-import { Toaster } from "@/components/ui/toaster";
+import { BiSolidDashboard } from "react-icons/bi";
+import {
+  BsDownload,
+  BsFolder,
+  BsLayoutSidebar,
+  BsWindowSidebar,
+} from "react-icons/bs";
+import BrowseByProducts from "./BrowseByProducts";
+
 const Home = () => {
   return (
-    <VStack spacing={16} mt={"24"} mx={{ lg: "72" }} align="stretch">
+    <VStack spacing={16} mt={"24"} mx={{ base: "4", lg: "72" }} align="stretch">
       <Box>
-        <HStack rounded={"xl"} bg={"#e9f7f7"} w={"full"}>
-          <VStack p={"16"}>
+        <Stack
+          rounded={"xl"}
+          bg={"#e9f7f7"}
+          w={"full"}
+          direction={{ base: "column", lg: "row" }}
+        >
+          <VStack p={"16"} align="stretch">
             <Text fontWeight={"bold"} fontSize={"xl"} alignSelf={"start"}>
               Get Started with Conduktor
             </Text>
@@ -26,8 +35,7 @@ const Home = () => {
               Learn how to install Conduktor and explore our Enterprise Data
               Management Platform for Streaming.
             </Text>
-            <HStack>
-              {" "}
+            <Stack direction={{ base: "column", md: "row" }} spacing={4} mt={4}>
               <Button
                 variant={"plain"}
                 rounded={"md"}
@@ -40,20 +48,106 @@ const Home = () => {
               <Button variant={"plain"} fontWeight={"bold"}>
                 Start with proxy
               </Button>
-            </HStack>
+            </Stack>
           </VStack>
           <Image
-            w={"1/2"}
+            w={{ base: "full", lg: "1/2" }}
             src="img.png"
-            mt={"16"}
+            mt={{ base: 2, lg: "16" }}
             rounded={"xl"}
             border={"xl"}
             borderColor={"#d9ffff"}
             borderRight={"none"}
             borderBottom={"none"}
           />
-        </HStack>
+        </Stack>
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 3,
+          }}
+          gap={4}
+          w={"full"}
+          mt={"8"}
+        >
+          <VStack
+            rounded={"xl"}
+            bg={"#e9f7f7"}
+            w={"full"}
+            alignItems={"start"}
+            alignContent={"space-between"}
+          >
+            <Box p={"6"}>
+              <Text
+                bg={"white"}
+                shadow={"lg"}
+                p={"4"}
+                rounded={"xl"}
+                fontWeight={"bolder"}
+              >
+                <BsWindowSidebar fontWeight={"bolder"} />
+              </Text>
+            </Box>
+            <Box p={"6"}>
+              <Text fontSize={"2xl"}>Platform Overview</Text>
+              <Text fontSize={"lg"}>
+                Explore Conduktor, its architecture and feature set
+              </Text>
+            </Box>
+          </VStack>
+          <VStack
+            rounded={"xl"}
+            bg={"#e9f7f7"}
+            w={"full"}
+            alignItems={"start"}
+            alignContent={"space-between"}
+          >
+            <Box p={"6"}>
+              <Text
+                bg={"white"}
+                shadow={"lg"}
+                p={"4"}
+                rounded={"xl"}
+                fontWeight={"bolder"}
+              >
+                <BsDownload className="" fontWeight={"bolder"} />
+              </Text>
+            </Box>
+            <Box p={"6"}>
+              <Text fontSize={"2xl"}>Implement Conducktor</Text>
+              <Text fontSize={"lg"}>
+                Get step-by-step guidance on deploying Conduktor efficiently
+              </Text>
+            </Box>
+          </VStack>
+          <VStack
+            rounded={"xl"}
+            bg={"#e9f7f7"}
+            w={"full"}
+            alignItems={"start"}
+            alignContent={"space-between"}
+          >
+            <Box p={"6"}>
+              <Text
+                bg={"white"}
+                shadow={"lg"}
+                p={"4"}
+                rounded={"xl"}
+                fontWeight={"bolder"}
+              >
+                <BsFolder fontWeight={"bolder"} />
+              </Text>
+            </Box>
+            <Box p={"6"}>
+              <Text fontSize={"2xl"}>Reference</Text>
+              <Text fontSize={"lg"}>
+                Browse our CLI and API documentation Terraform and more.
+              </Text>
+            </Box>
+          </VStack>
+        </SimpleGrid>
       </Box>
+      <BrowseByProducts />
     </VStack>
   );
 };
